8b44be941a1d1ebd98737b2ffb930bcf5eca0163
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2024-03-15 10:11:59 +0000

    Adding Abstraction structures in the codebase
    appling imported module to create intuitive output

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import sysInfo_Win
 
 system_Info = sysInfo_Win.get_sys_info()
 
-# printing the details 
-for i, j in system_Info.items(): 
-  print(f"{i.title()}, - {j}") 
+def main(info):
+  """
+  This Python script will output the system information of the computer that it is running on.
+  """
+  print("""
+---------------------------------
+  System Information
+---------------------------------
+  """)
+  # printing the details 
+  for i, j in info.items(): 
+    print(f"{i.title()}: {j}") 
+  
+  print("""
+---------------------------------
+  """)
+
+
+if __name__ == "__main__":
+  main(system_Info)
